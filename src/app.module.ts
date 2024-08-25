6a0cb1eaa7f3c2615360506065bcf38e98c6ea01
Vahid Nejad <vahid884@gmail.com>
2024-08-25 12:32:51 +0330

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { PostModule } from './post/post.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     DrizzleModule,
     PostModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
